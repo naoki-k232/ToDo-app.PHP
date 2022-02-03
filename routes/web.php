47462b8b5a40cfda88aad5733bc8d5ebe1a1e712
naoki-k232 <74887536+naoki-k232@users.noreply.github.com>
2022-02-03 16:14:43 +0900

    コントローラーまで完了

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/', 'TaskController@index');
+Route::get('/index', 'TaskController@index');
